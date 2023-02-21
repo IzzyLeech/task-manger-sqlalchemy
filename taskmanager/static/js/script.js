3e9f4b0bfea2b395f9c3a71b2a2cfb674820b3a4
IzzyLeech <ralphhunter46@gmail.com>
2023-02-21 13:27:25 +0000

    add the adding task

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,17 @@
 document.addEventListener('DOMContentLoaded', function (){
-
+    // sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // datepicker initializtion
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format:"dd mmmm, yyyy",
+        i18n: {done: "Select"}
+    })
+
+    // select initializtion
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+
 });
